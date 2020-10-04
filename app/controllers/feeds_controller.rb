a03c79a91ea4c088aaadf14db59a0288bea64f5e
yuichi oyama <yuichi19991208@gmail.com>
2020-10-04 15:20:53 +0900

    userの登録機能を作成した

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -66,4 +66,5 @@ class FeedsController < ApplicationController
   def feed_params
     params.require(:feed).permit(:image, :content, :image_cache)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root to: 'feeds#index'
-
   resources :feeds do
+    collection do
+      post :confirm
+    end
+  end
+
+  resources :users do
     collection do
       post :confirm
     end
