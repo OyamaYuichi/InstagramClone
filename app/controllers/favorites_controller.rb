d50e330a8073e368c74285242298a51d33bca5a2
yuichi oyama <yuichi19991208@gmail.com>
2020-10-08 00:37:06 +0900

    お気に入りボタンの修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,17 +2,17 @@ class FavoritesController < ApplicationController
   
   def index
       @user = User.find(params[:user_id])
-      @favorites = @user.favorites
+      @favorites = @user.favorites.order(created_at: :desc)
   end
   
   def create
     favorite = current_user.favorites.create(feed_id: params[:feed_id])
-    redirect_to feeds_url, notice: "#{favorite.feed.user.name}さんの投稿をいいね！しました"
+    redirect_to feed_path(params[:feed_id]), notice: "#{favorite.feed.user.name}さんの投稿をいいね！しました"
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to feeds_url, notice: "#{favorite.feed.user.name}さんの投稿のいいね！を解除しました"
+    redirect_to feed_path(favorite.feed.id), notice: "#{favorite.feed.user.name}さんの投稿のいいね！を解除しました"
   end
 
   
